fffca10044c8a736a9dc4b521debcccde43e511b
jih111 <zhfwlgp@naver.com>
2021-10-04 09:43:40 +0900

    firebase setting

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
 function App() {
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
   return <></>;
 }
 
